95485113e5aa644fb9a38e480e5ea5e3dc270892
Marko Leskovsek <marko.leskovsek.91@gmail.com>
2023-12-09 09:25:57 +0100

    Added screen bounds for ship

--- start/Model.lua
+++ start/Model.lua
@@ -34,5 +34,14 @@ Model.init = function()
 
 end
 
+Model.getScreenBoundries = function(x,y,w,h)
+    return (
+        (x < Model.stage.stageWidth - w) 
+        and ( 0  < x)
+        and (y < Model.stage.stageHeight - h)
+        and (0 < y)
+    )
+end
+
 
 return Model
--- start/Ship.lua
+++ start/Ship.lua
@@ -6,10 +6,12 @@ function Ship:init(params)
     print("Ship init!")
     self.speed = params.speed
     self.asset = params.asset
-    self.x = Model.stage.stageWidth / 2
-    self.y = Model.stage.stageHeight / 2
+    self.x = Model.stage.stageWidth / 2 -- this will put ship in center of screen X
+    self.y = Model.stage.stageHeight / 2 -- this will put ship in center of screen Y
     self.w = self.asset:getWidth()
     self.h = self.asset:getHeight()
+
+    isInBoundries = false
 end
 
 function Ship:update(dt)
@@ -36,15 +38,29 @@ function Ship:update(dt)
         y = y + 1
     end
 
-    self.x = self.x + (x * self.speed * dt)
-    self.y = self.y + (y * self.speed * dt)
+    local x1 = self.x + (x * self.speed * dt)
+    local y1 = self.y + (y * self.speed * dt)
+
+    local newX , newY = screenCoordinates(x1, y1, self.w, self.h)
+    isInBoundries = Model.getScreenBoundries(newX, newY, self.w, self.h)
+
+    if isInBoundries then
+        self.x = self.x + (x * self.speed * dt)
+        self.y = self.y + (y * self.speed * dt)
+    end
+
 
 end
 
 function Ship:draw()
-    local newX = self.x - (self.w/2)
-    local newY = self.y - (self.h/2)
+    local newX , newY = screenCoordinates(self.x, self.y, self.w, self.h)
     love.graphics.draw(self.asset, newX,newY )
+end
+
+function screenCoordinates(x,y,w,h) -- offset ship graphics up-left
+    local newX = x - (w/2)
+    local newY = y - (h/2)
+    return newX, newY
 end
 
 return Ship
