1f2c9077b80793dc381985a2c3be13a6b1b9a20c
Marko Leskovsek <marko.leskovsek.91@gmail.com>
2023-12-11 16:01:10 +0100

    Collision will now return index of first collided object

--- start/Collision.lua
+++ start/Collision.lua
@@ -1,23 +1,23 @@
 local classes = require("classes")
 local Collision = classes.class()
 local Utils = require("Utils")
-local Vector = require("Vector")
 
 function Collision:checkCollision(x, y)
     -- we check if rects overlap
     for i=1, Utils.tablelength(y) do
         if self:rectOverlap(x, y[i]) then
-            return true
+            return i
         end
     end
 
-    return false
+    return -1
 end
 
 function Collision:valueInRange(value, min, max)
     return value >= min and value <= max 
 end
 
+-- using rects for simplicity
 function Collision:rectOverlap(rectA, rectB)
     local xOverlap = self:valueInRange(rectA.position.x, rectB.position.x, rectB.position.x + rectB.w)
         or self:valueInRange(rectB.position.x, rectA.position.x, rectA.position.x + rectA.w);
